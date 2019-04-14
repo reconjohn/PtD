2b178a8be107863a4daa0a0fafeca5e66c689a48
Yohan <min25@uw.edu>
2019-04-14 13:36:00 -0700

    report draft

--- syntax/survey.R
+++ syntax/survey.R
@@ -17,25 +17,29 @@ df <- read_csv('./data/survey.csv')
 View(df)
 daf <- df %>%
   gather(feature, option, area_1:mount_3, na.rm = T) %>%
-  mutate(criteria = ifelse(str_detect(feature, "1"), "safety",
-                           ifelse(str_detect(feature, "2"), "applicability", 'cost-effectiveness')),
+  mutate(criteria = ifelse(str_detect(feature, "1"), "Safety",
+                           ifelse(str_detect(feature, "2"), "Applicability", 'Cost-effect')),
          feature = str_replace_all(feature, "_[0-9]", "")) %>%
   select(ID, pos, criteria, feature, option) %>%
   mutate(criteria = parse_factor(criteria,
-                                 levels = c('safety', 'applicability', 'cost-effectiveness')),
+                                 levels = c('Safety', 'Applicability', 'Cost-effect')),
          feature = parse_factor(feature,
                                 levels = c('area', 'material', 'pitch', 'fall', 'access',
                                            'conduit', 'inverter', 'mount')),
-         pos = parse_factor(pos, levels = c('solar_contractor', 'contractor', 'designer',
-                                            'electrical')))
+         position = parse_factor(pos, levels = c('solar_contractor', 'contractor', 'designer',
+                                            'electrical'))) %>%
+  select(-pos)
 View(daf)
 
-g_ov_sur <- ggplot(daf, aes(x = option, fill = pos)) +
+g_ov_sur <- ggplot(daf, aes(x = option, fill = position)) +
   facet_grid(criteria ~ feature) +
   geom_bar(width = 0.8) +
   scale_x_discrete(limits = c(1:4)) +
   scale_y_continuous(breaks = seq(0, 11, len = 12)) +
-  theme_bw() + theme(legend.position = c(0.11, 0.2),
+  theme_bw() + theme(legend.position = c(0.79, 0.27),
+                     strip.text = element_text(size = 17),
+                     legend.text = element_text(size = 15),
+                     text = element_text(size = 17),
                      legend.background = element_rect(color = 1))
 
 
